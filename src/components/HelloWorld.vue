1111525461fe65d85821257dbc50dff83ce782d6
zxfjd3g <258147149@qq.com>
2018-12-13 11:23:40 +0800

    init app

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2 class="title">{{msg}}</h2>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: "First Vue Component222"
+      }
+    }
+  }
+</script>
+
+<style>
+  .title {
+    color: red;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+/*
+入口JS
+ */
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App/>'
+})
